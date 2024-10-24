0512f997d70bceec21a391daebb845d387731dfa
Michael Huckabay <danielrosendahl70@gmail.com>
2024-10-24 15:33:34 -0500

    123123

--- fcp-microservices/services/accountcontroller/main.py
+++ fcp-microservices/services/accountcontroller/main.py
@@ -44,7 +44,7 @@ def get_entities(api_key, message):
         "role": "system",
         "content": """
         You are an expert in extracting key information from a given user prompt.
-        Okay, that would be okay, 
+        Hello, sorry here using phone is very difficult. are you available this weekend?
 The user's prompt will be in the subject area of Accounts Receivables. 
 The primary objective of the user prompt is to extract information from Accounting System. 
 The Accounting system belongs to the Company Doordash.
